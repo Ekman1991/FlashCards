61e28695869660466a91be5c8867d9d1e48796cd
Philip Ekman <ekman1991@gmail.com>
2016-04-22 17:49:43 +0200

    Added FlashCards to our Singleton

--- FlashCards/app/src/main/java/se/tda367/flashcards/Singleton.java
+++ FlashCards/app/src/main/java/se/tda367/flashcards/Singleton.java
@@ -1,15 +1,38 @@
 package se.tda367.flashcards;
 
+import android.content.Context;
+
 /**
  * Created by Razzan on 2016-04-21.
  */
 public class Singleton {
-    private static Singleton flashCards = new Singleton();
+
+    private static Singleton singleton = null;
+    private FlashCards flashCards;
+    private DatabaseController dbController;
+
+    protected Singleton() {
+        flashCards = new FlashCards();
+    }
 
     public static Singleton getInstance() {
-        return flashCards;
+        if(singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
     }
 
-    private Singleton() {
+    public FlashCards getFlashCards() {
+        return this.flashCards;
     }
+
+    public DatabaseController getDatabaseController(Context context) {
+        if (dbController == null) {
+            dbController = new DatabaseController(context);
+            return dbController;
+        } else {
+            return dbController;
+        }
+    }
+
 }
