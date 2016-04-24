39dceb3d8d95fb86014d3e70844efcff499e6c5c
Philip Ekman <ekman1991@gmail.com>
2016-04-24 12:47:59 +0200

    Added createCard to a Deck

--- FlashCards/app/src/main/java/se/tda367/flashcards/Deck.java
+++ FlashCards/app/src/main/java/se/tda367/flashcards/Deck.java
@@ -16,7 +16,9 @@ public class Deck{
     private int counter;
 
     public Deck() {
-
+        this.name = "";
+        this.list = new ArrayList<Card>();
+        this.counter = 0;
     }
 
     public Deck(String name) {
@@ -51,6 +53,10 @@ public class Deck{
 
     public void addCard(Card card){
         list.add(card);
+    }
+
+    public void setCardsArray(ArrayList<Card> cards) {
+        this.list = cards;
     }
 
     public Card getNextCard() {
