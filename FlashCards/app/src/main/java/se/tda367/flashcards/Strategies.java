fd8081172f0f7ae3c1399f69815334ac00425d36
Emilia Vestlund <emiliavestlund@hotmail.com>
2016-04-26 16:23:44 +0200

    Starting of strategies

---
+++ FlashCards/app/src/main/java/se/tda367/flashcards/Strategies.java
@@ -0,0 +1,27 @@
+package se.tda367.flashcards;
+
+/**
+ * Created by Emilia on 2016-04-26.
+ */
+public class Strategies {
+    private Strategies strategi;
+
+    public Strategies (){
+
+    }
+
+    public Strategies(Strategies strategi){
+        this.strategi = strategi;
+    }
+
+    public void setStrategi (Strategies strategi){
+        this.strategi = strategi;
+    }
+
+    public Strategies getStrategie(){
+        return this.strategi;
+    }
+
+
+
+}
---
+++ FlashCards/app/src/main/java/se/tda367/flashcards/StrategyAllCards.java
@@ -0,0 +1,16 @@
+package se.tda367.flashcards;
+
+/**
+ * Created by Emilia on 2016-04-26.
+ */
+public class StrategyAllCards extends Strategies{
+    public StrategyAllCards(){
+        Strategies StrategyAllCards = new Strategies();
+
+        
+    }
+
+
+
+
+}
