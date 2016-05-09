7b3215748bde757392e83a72d53e655aa81b26e1
Emilia Vestlund <emiliavestlund@hotmail.com>
2016-05-09 21:26:45 +0200

    audio start

---
+++ FlashCards/app/src/main/java/se/tda367/flashcards/controllers/AudioActivity.java
@@ -0,0 +1,25 @@
+package se.tda367.flashcards.controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import se.tda367.flashcards.CardFactory;
+import se.tda367.flashcards.R;
+
+/**
+ * Created by Emilia on 2016-05-09.
+ */
+public class AudioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+
+
+    }
+
+}
