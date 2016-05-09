16ae63646688d58e8b33e3a689a2b8c6c3b356aa
emilhu <emil.hukic@hotmail.com>
2016-05-09 23:03:07 +0200

    Statistics Activity Added
    Added a button to the toolbar that opens the statistics activity

---
+++ FlashCards/app/src/main/java/se/tda367/flashcards/controllers/StatisticsActivity.java
@@ -0,0 +1,15 @@
+package se.tda367.flashcards.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import se.tda367.flashcards.R;
+
+public class StatisticsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_statistics);
+    }
+}
