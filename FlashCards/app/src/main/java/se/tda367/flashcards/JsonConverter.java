1dcfd954de1e09583f60839e011f62c9acd19337
Rstrandell <razmus@student.chalmers.se>
2016-05-12 16:46:02 +0200

    gör om en deck till ett JsonObjekt innehållande korten i form av en JsonArray med en metod som returnerar objektet på String form

---
+++ FlashCards/app/src/main/java/se/tda367/flashcards/JsonConverter.java
@@ -0,0 +1,31 @@
+package se.tda367.flashcards;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import se.tda367.flashcards.models.Deck;
+
+/**
+ * Created by Razzan on 2016-05-12.
+ */
+public class JsonConverter {
+    JSONObject writer = new JSONObject();
+    JSONArray list = new JSONArray();
+
+    public JsonConverter(Deck deck) throws Exception{
+
+        for(int i = 0; i<deck.getSize(); i++){
+            list.put(i, deck.getList().get(i));
+        }
+
+        writer.put("name", deck.getName());
+        writer.put("list", list);
+       /* writer.put("counter", deck.getCounter());
+        writer.put("playedSince", deck.getPlayedSince());
+        writer.put("nbrOfTimesPlayed", deck.getNbrOfTimesPlayed());
+        writer.put("made", deck.getMade());*/
+    }
+    public String getJsonString(){
+        return this.writer.toString();
+    }
+}
